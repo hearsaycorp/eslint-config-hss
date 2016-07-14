693bc41545e8c44d92eb2519f67ebfa491637f70
Jordan Eldredge <jeldredge@hearsaycorp.com>
2016-07-14 01:14:18 +0000

    Add tests
    Inspired by those in `eslint-config-standard`

---
+++ test/basic.js
@@ -0,0 +1,11 @@
+var config = require('../')
+var test = require('tape')
+
+test('test basic properties of config', function (t) {
+  t.ok(isObject(config.rules))
+  t.end()
+})
+
+function isObject (obj) {
+  return typeof obj === 'object' && obj !== null
+}
