b8e035170bf79f917b1647e53c811b234e367843
Jordan Eldredge <jordan@jordaneldredge.com>
2016-08-29 14:00:52 -0700

    Add default rules for ES6
    We test-ran these rules with the Hearsay Messages Web Client project. They
    seemed to work well there, so think they are ready for adoption by other
    projects.

---
+++ es6.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "rules": {
+        "arrow-spacing": "error",
+        "constructor-super": "error",
+        "generator-star-spacing": "error",
+        "no-class-assign": "error",
+        "no-confusing-arrow": "error",
+        "no-const-assign": "error",
+        "no-dupe-class-members": "error",
+        "no-duplicate-imports": "error",
+        "no-new-symbol": "error",
+        "no-restricted-imports": "error",
+        "no-this-before-super": "error",
+        "no-useless-computed-key": "error",
+        "no-useless-constructor": "error",
+        "no-useless-rename": "error",
+        "no-var": "error",
+        "prefer-const": "error",
+        "prefer-reflect": "error",
+        "prefer-rest-params": "error",
+        "prefer-spread": "error",
+        "require-yield": "error",
+        "rest-spread-spacing": "error",
+        "template-curly-spacing": "error",
+        "yield-star-spacing": "error"
+    }
+}
