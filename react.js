4b0d1788a63c5ec2e575e90b295e6be323ea454d
Jordan Eldredge <jeldredge@hearsaycorp.com>
2016-11-29 01:27:18 +0000

    Add React config

---
+++ react.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "plugins": [
+        "react"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "rules": {
+      "react/display-name": "error",
+      "react/jsx-boolean-value": ["error", "always"],
+      "react/jsx-indent": "error",
+      "react/jsx-indent-props": "error",
+      "react/jsx-key": "error",
+      "react/jsx-no-comment-textnodes": "error",
+      "react/jsx-no-duplicate-props": "error",
+      "react/jsx-no-undef": "error",
+      "react/jsx-pascal-case": "error",
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "react/no-children-prop": "error",
+      "react/no-danger-with-children": "error",
+      "react/no-deprecated": "error",
+      "react/no-did-mount-set-state": "error",
+      "react/no-did-update-set-state": "error",
+      "react/no-direct-mutation-state": "error",
+      "react/no-is-mounted": "error",
+      "react/no-render-return-value": "error",
+      "react/no-string-refs": "error",
+      "react/no-unescaped-entities": "error",
+      "react/no-unknown-property": "error",
+      "react/no-unused-prop-types": "error",
+      "react/prefer-es6-class": ["error", "always"],
+      "react/react-in-jsx-scope": "error",
+      "react/require-render-return": "error",
+      "react/style-prop-object": "error"
+    }
+}
